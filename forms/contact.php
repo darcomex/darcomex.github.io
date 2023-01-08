82632f600698b62b2c3600f49f4483ae705ae04b
dacomex <mobmarwan@gmail.com>
2023-01-08 04:01:40 +0100

    changement cv

--- forms/contact.php
+++ forms/contact.php
@@ -7,7 +7,6 @@
   */
 
   // Replace contact@example.com with your real receiving email address
-  $receiving_email_address = 'mrsmmellouk@gmail.com';
 
   if( file_exists($php_email_form = '../assets/vendor/php-email-form/php-email-form.php' )) {
     include( $php_email_form );
@@ -15,27 +14,20 @@
     die( 'Unable to load the "PHP Email Form" Library!');
   }
 
-  $contact = new PHP_Email_Form;
-  $contact->ajax = true;
-  
-  $contact->to = $receiving_email_address;
-  $contact->from_name = $_POST['name'];
-  $contact->from_email = $_POST['email'];
-  $contact->subject = $_POST['subject'];
+  if(!empty($_POST["submit"])) {
+    $name = $_POST["name"];
+    $email = $_POST["email"];
+    $subject = $_POST["subject"];
+    $message = $_POST["message"];
+    $toEmail = "mrsmmellouk@gmail.com";
+    $mailHeaders = "From: " . $name . "<". $email .">\r\n";
+    if(mail($toEmail, $subject, $message, $mailHeaders)) {
+      $mail_msg = "Vos informations de contact ont été reçues avec succés.";
+      $type_mail_msg = "success";
+    }else{
+      $mail_msg = "Erreur lors de l'envoi de l'e-mail.";
+      $type_mail_msg = "error";
+    }
+  }
 
-  // Uncomment below code if you want to use SMTP to send emails. You need to enter your correct SMTP credentials
-  /*
-  $contact->smtp = array(
-    'host' => 'example.com',
-    'username' => 'example',
-    'password' => 'pass',
-    'port' => '587'
-  );
-  */
-
-  $contact->add_message( $_POST['name'], 'From');
-  $contact->add_message( $_POST['email'], 'Email');
-  $contact->add_message( $_POST['message'], 'Message', 10);
-
-  echo $contact->send();
 ?>
